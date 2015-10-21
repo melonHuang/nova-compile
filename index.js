2543d4cf7b55ae18b9aa5743220d778b1d1fdf5c
melonHuang <melonh0327@gmail.com>
2015-10-21 13:56:28 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,171 @@
+'user strict';
+
+var through = require('through2');
+var fs = require('fs');
+var path = require('path');
+var cheerio = require('cheerio');
+var umdWrap = require('umd-wrap');
+var assign = Object.assign || require('object.assign');
+var extend = require('extend');
+var CleanCSS = require('clean-css');
+var autoprefixer = require('autoprefixer');
+var postcss = require('postcss');
+var prefixer = postcss([autoprefixer]);
+var slash = require('slash');;
+var requirejs = require('requirejs');
+var temp = require('temp').track();
+var amdclean = require('amdclean');
+var utils = require('./utils');
+var Promise = require('promise');
+
+var i = 0;
+
+module.exports = {
+    compile: compile
+};
+
+
+function compile(file, option) {
+    return new Promise(function(resolve, reject) {
+        var source = new Buffer(file.contents, 'utf8').toString();
+        var $ = cheerio.load(source);
+        var domModule = $('template[is=dom-module]');
+
+        if(domModule.length == 0) {
+            reject('no dom-module found');
+            return;
+        };
+
+        htmlToJs($).then(function(script) {
+            var defaultOpt = {
+                umd: {
+                    exports: function(file) {
+                        var dirpath = path.dirname(path.resolve(file.path)).split(path.sep);
+                        var lastFoldName = dirpath[dirpath.length - 1];
+                        return utils.capitalize(utils.dashToCamelCase(lastFoldName));
+                    },
+                    root: function(file) {
+                        return 'window';
+                    }
+                }
+            };
+            var opt = extend({}, defaultOpt, option);
+
+            var dependencies = parseDependencies($);
+
+            // wrap umd
+            // combo的实现依赖于UMD
+            if(opt.umd || opt.combo) {
+                var options = {
+                    code: script,
+                    exports: getExport($) || (opt.umd && opt.umd.exports(file)) || defaultOpt.umd.exports(file),
+                    dependencies: dependencies
+                }
+
+                umdWrap(options, function(err, wrappedScript) {
+                    wrappedScript = '(function() {' + wrappedScript + '}).call(' + (opt.umd && opt.umd.root() || defaultOpt.umd.root()) + ')';
+
+                    if(opt.combo) {
+                        combo({
+                            file: file,
+                            code: wrappedScript,
+                            dependencies: dependencies,
+                            baseUrl: opt.combo.baseUrl,
+                            // 控制combo后，是否保留最外层的UMD Wrap
+                            keepUmd: !!opt.umd
+                        }).then(function(cleanedScript) {
+                            resolve(cleanedScript);
+                        });
+                    } else {
+                        resolve(wrappedScript);
+                    }
+                });
+            } else {
+                resolve(script);
+            }
+        }).catch(function(e) {
+            console.log(e);
+        });
+    });
+}
+
+
+function htmlToJs($) {
+    var domModule = $('template[is=dom-module]');
+
+
+    var style = domModule.children('style').html() || '';
+    var template = domModule.children('template').html() || '';
+    var script = domModule.children('script:not([require-src])').html();
+
+    id = domModule.attr('id');
+
+    // clean and prefix css
+    return prefixer.process(style).then(function(result) {
+        var exports = {
+            stylesheet: new CleanCSS().minify(result.css).styles,
+            template: template
+        }
+
+        script = script.replace('Nova(', 'NovaExports(');
+        script = 'undefined;NovaExports.__fixedUglify="script>";' + 'NovaExports.exports=' + JSON.stringify(exports).replace(/<\/script>/g, '</" + NovaExports.__fixedUglify + "') + ';' + script;
+
+
+        return script;
+
+    });
+}
+
+
+var exportsCount = 0;
+function parseDependencies($, extra) {
+    var domModule = $('template[is=dom-module]');
+    var depEls = domModule.children('link[rel=import],script[require-src]');
+    var depArr = [];
+    depEls.each(function(i, depEle) {
+        var name = '';
+        name = $(depEle).attr('require-src').trim();
+
+        var pathArr = name.split(path.sep);
+        var exports = $(depEle).attr('exports') || '_' + ++exportsCount;
+        depArr.push({
+            name: slash(name),
+            exports: exports
+        });
+    });
+    return depArr;
+}
+
+function getExport($) {
+    var domModule = $('template[is=dom-module]');
+    var script = domModule.children('script:not([require-src])');
+    return script.attr('exports');
+}
+
+function combo(opt) {
+    var tmpSource = temp.openSync({ suffix: '.js'});
+    var sourcePath = opt.file.path.replace(/.html$/, '.js');
+    var tmpFile = temp.openSync({ suffix: '.js'});
+    fs.writeFileSync(sourcePath, opt.code, 'utf-8');
+    var baseUrl = opt.baseUrl;
+    var config = {
+        baseUrl: baseUrl,
+        name: path.relative(baseUrl, sourcePath.replace(/.js/, '')),
+        out: path.relative(baseUrl, tmpFile.path),
+        onModuleBundleComplete: function (data) {
+            outputFile = data.path,
+            cleanedCode = amdclean.clean({
+                'filePath': outputFile,
+                'transformAMDChecks': !opt.keepUmd
+            });
+            fs.writeFileSync(outputFile, cleanedCode);
+        }
+    }
+    return new Promise(function(resolve) {
+        requirejs.optimize(config, function() {
+            var content = fs.readFileSync(config.out, 'utf8');
+            resolve(content);
+        });
+    });
+}
+
